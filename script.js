021465e2d6cecccbcbc30172e861189934a06520
DerMoehre <94791958+DerMoehre@users.noreply.github.com>
2024-03-03 11:02:42 +0100

    added read toggle

--- script.js
+++ script.js
@@ -13,6 +13,55 @@ const myLibrary = [
     }
 ];
 
+function clearInput () {
+    document.getElementById('input_title').value = '';
+    document.getElementById('input_author').value = '';
+    document.getElementById('input_page').value = '';
+    document.getElementById('input_read').checked = false;
+}
+
+function toggleDisplay () {
+    let bgContainer = document.querySelector('.bg_container');
+    bgContainer.classList.toggle('d_none');
+}
+
+function toggleRead (index) {
+    myLibrary[index].read  = !myLibrary[index].read;
+    renderBook();
+}
+
+function closeButton () {
+    let closeButton = document.querySelector('.btn_remove');
+    closeButton.addEventListener('click', () => {
+        toggleDisplay();
+        clearInput();
+    });
+}
+
+function openInput () {
+    let addButton = document.querySelector('.new_book');
+    addButton.addEventListener('click', () => {
+        toggleDisplay();
+    });
+}
+
+function submit() {
+    let readStatus = false;
+    let submitButton = document.querySelector('.btn_submit');
+    submitButton.addEventListener('click', () => {
+        let title = document.getElementById('input_title').value;
+        let author = document.getElementById('input_author').value;
+        let page = document.getElementById('input_page').value;
+        let read = document.getElementById('input_read');
+        if (read.checked) {
+            readStatus = true;
+        }
+    addBookToLibrary(title, author, page, readStatus);
+    clearInput();
+    toggleDisplay();
+    });
+}
+
 function Book(title, author, page, read) {
   this.title = title;
   this.author = author;
@@ -23,25 +72,30 @@ function Book(title, author, page, read) {
 function addBookToLibrary(title, author, page, read) {
     const newBook = new Book(title, author, page, read);
     myLibrary.push(newBook);
+    renderBook();
 }
 
 function renderBook() {
     let bookContent = document.querySelector('.book_content');
+    bookContent.innerHTML = '';
     for (let i = 0; i < myLibrary.length; i++) {
-        bookContent.innerHTML += generateBookCardHtml(myLibrary[i].title, myLibrary[i].author, myLibrary[i].page, myLibrary[i].read);
+        bookContent.innerHTML += generateBookCardHtml(i, myLibrary[i].title, myLibrary[i].author, myLibrary[i].page, myLibrary[i].read);
     }
 }
 
-function generateBookCardHtml(title, author, page, read) {
+function generateBookCardHtml(index, title, author, page, read) {
     return `
     <div class="book">
         <p class="title">${title}</p>
         <p class="author">${author}</p>
         <p class="pages">${page}</p>
-        <button class="read ${read}">Read</button>
+        <button onclick="toggleRead(${index})" class="read ${read}">Read</button>
         <button class="remove">Remove</button>
     </div>
 `;
 }
 
+openInput();
+submit();
+closeButton();
 renderBook();
